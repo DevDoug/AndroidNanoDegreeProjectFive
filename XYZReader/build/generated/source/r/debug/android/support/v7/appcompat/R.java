0fe8cbc6057bf1f2451ddea7a7ec0cf0c6a9f4da
DevDoug <Doug4less@gmail.com>
2016-02-18 21:58:38 -0800

    Changed the toolbar back to default beacuse it was not working with app bar layout and coordinator layout.

--- XYZReader/build/generated/source/r/debug/android/support/v7/appcompat/R.java
+++ XYZReader/build/generated/source/r/debug/android/support/v7/appcompat/R.java
@@ -429,7 +429,7 @@ public final class R {
 		public static final int notification_template_icon_bg = 0x7f020049;
 	}
 	public static final class id {
-		public static final int action0 = 0x7f0a007f;
+		public static final int action0 = 0x7f0a007e;
 		public static final int action_bar = 0x7f0a0057;
 		public static final int action_bar_activity_content = 0x7f0a0000;
 		public static final int action_bar_container = 0x7f0a0056;
@@ -438,7 +438,7 @@ public final class R {
 		public static final int action_bar_subtitle = 0x7f0a003b;
 		public static final int action_bar_title = 0x7f0a003a;
 		public static final int action_context_bar = 0x7f0a0058;
-		public static final int action_divider = 0x7f0a0083;
+		public static final int action_divider = 0x7f0a0082;
 		public static final int action_menu_divider = 0x7f0a0002;
 		public static final int action_menu_presenter = 0x7f0a0003;
 		public static final int action_mode_bar = 0x7f0a0054;
@@ -449,9 +449,9 @@ public final class R {
 		public static final int always = 0x7f0a002b;
 		public static final int beginning = 0x7f0a0029;
 		public static final int buttonPanel = 0x7f0a004d;
-		public static final int cancel_action = 0x7f0a0080;
+		public static final int cancel_action = 0x7f0a007f;
 		public static final int checkbox = 0x7f0a004f;
-		public static final int chronometer = 0x7f0a0086;
+		public static final int chronometer = 0x7f0a0085;
 		public static final int collapseActionView = 0x7f0a002c;
 		public static final int contentPanel = 0x7f0a0048;
 		public static final int custom = 0x7f0a004c;
@@ -463,7 +463,7 @@ public final class R {
 		public static final int dropdown = 0x7f0a0031;
 		public static final int edit_query = 0x7f0a0059;
 		public static final int end = 0x7f0a0020;
-		public static final int end_padder = 0x7f0a008b;
+		public static final int end_padder = 0x7f0a008a;
 		public static final int expand_activities_button = 0x7f0a003e;
 		public static final int expanded_menu = 0x7f0a004e;
 		public static final int home = 0x7f0a0004;
@@ -471,12 +471,12 @@ public final class R {
 		public static final int icon = 0x7f0a0042;
 		public static final int ifRoom = 0x7f0a002d;
 		public static final int image = 0x7f0a003f;
-		public static final int info = 0x7f0a008a;
-		public static final int line1 = 0x7f0a0084;
-		public static final int line3 = 0x7f0a0088;
+		public static final int info = 0x7f0a0089;
+		public static final int line1 = 0x7f0a0083;
+		public static final int line3 = 0x7f0a0087;
 		public static final int listMode = 0x7f0a000a;
 		public static final int list_item = 0x7f0a0041;
-		public static final int media_actions = 0x7f0a0082;
+		public static final int media_actions = 0x7f0a0081;
 		public static final int middle = 0x7f0a002a;
 		public static final int multiply = 0x7f0a0035;
 		public static final int never = 0x7f0a002e;
@@ -507,13 +507,13 @@ public final class R {
 		public static final int src_atop = 0x7f0a0037;
 		public static final int src_in = 0x7f0a0038;
 		public static final int src_over = 0x7f0a0039;
-		public static final int status_bar_latest_event_content = 0x7f0a0081;
+		public static final int status_bar_latest_event_content = 0x7f0a0080;
 		public static final int submit_area = 0x7f0a0062;
 		public static final int tabMode = 0x7f0a000c;
-		public static final int text = 0x7f0a0089;
-		public static final int text2 = 0x7f0a0087;
+		public static final int text = 0x7f0a0088;
+		public static final int text2 = 0x7f0a0086;
 		public static final int textSpacerNoButtons = 0x7f0a004a;
-		public static final int time = 0x7f0a0085;
+		public static final int time = 0x7f0a0084;
 		public static final int title = 0x7f0a0043;
 		public static final int title_template = 0x7f0a0046;
 		public static final int topPanel = 0x7f0a0045;
